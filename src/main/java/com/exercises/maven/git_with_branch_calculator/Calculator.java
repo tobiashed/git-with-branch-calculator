0572a407495a080cfa88a2b650a88601924cbc5a
Tobias Hedkvist <tobias.hedkvist@hotmail.com>
2017-11-29 09:41:41 +0100

    finished the method add and created test for it

---
+++ src/main/java/com/exercises/maven/git_with_branch_calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.exercises.maven.git_with_branch_calculator;
+
+public class Calculator {
+	public int add(int firstNumber, int secondNumber) {
+		return firstNumber + secondNumber;
+	}
+}
---
+++ src/test/java/com/exercises/maven/git_with_branch_calculator/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.exercises.maven.git_with_branch_calculator;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculator c = new Calculator();
+	private static final Logger LOG = Logger.getLogger(Calculator.class.getName());
+
+	@Test
+	public void testAdd() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+
+		for (int i = 0; i < 10; i++) {
+			firstNumber = random.nextInt(100);
+			secondNumber = random.nextInt(100);
+			result = firstNumber + secondNumber;
+			LOG.info("Testing the method with " + firstNumber + " and " + secondNumber);
+			assertEquals(c.add(firstNumber, secondNumber), result);
+		}
+	}
+
+}
